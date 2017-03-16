c9628e1848cde0cdcf1ca04218148970bf756a82
nattyait <nattanicha@gmail.com>
2017-03-16 12:12:02 +0700

    Refactor addToTweet to BodyContainer :baby:

--- src/components/BodyContainer.js
+++ src/components/BodyContainer.js
@@ -1,28 +1,75 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import MainPanel from './MainPanel'
 import Profile from './Profile'
+import config from '../config'
 
-const BodyContainer = ({ ownerUsername, enableTweet }) => (
-  <div className="container body">
-    <div className="left-panel">
-      <Profile
-        name="Nattanicha"
-        username={ownerUsername}
-        numTweets={123}
-        numFollowers={123}
-        numFollowings={123}
-        isFollowing
-        isOwnProfile
-        handleToggleFollow={() => { console.log('toggle!!!!') }}
-      />
-    </div>
-    <MainPanel enableTweet={enableTweet} />
-  </div>
-)
+class BodyContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: 'Nattanicha',
+      username: 'nattyait',
+      tweets: [],
+    }
+    this.addToTweetList = this.addToTweetList.bind(this)
+  }
+  componentDidMount() {
+    const uri = `http://${config.api.host}:${config.api.port}/api/tweets`
+    const filter = `{"where":{"username":"${this.state.username}"}}`
 
-export default BodyContainer
+    fetch(`${uri}?filter=${filter}`, {
+      mode: 'cors',
+    })
+      .then(response => response.json())
+      .then((tweets) => {
+        this.setState({
+          tweets, // tweets: tweets
+        })
+      })
+  }
+  addToTweetList(tweet) {
+    const tweetWithId = tweet
+    tweetWithId.id = this.state.tweets.length
+
+    this.setState({
+      tweets: [
+        ...this.state.tweets,
+        tweetWithId,
+      ],
+    })
+  }
+  render() {
+    const { name, username, tweets } = this.state
+    return (
+      <div className="container body">
+        <div className="left-panel">
+          <Profile
+            name="Nattanicha"
+            username={this.props.ownerUsername}
+            numTweets={123}
+            numFollowers={123}
+            numFollowings={123}
+            isFollowing
+            isOwnProfile
+            handleToggleFollow={() => { console.log('toggle!!!!') }}
+          />
+        </div>
+        <MainPanel
+          enableTweet={this.props.enableTweet}
+          name={name}
+          username={username}
+          tweets={tweets}
+          addToTweetList={this.addToTweetList}
+        />
+      </div>
+    )
+  }
+}
 
 BodyContainer.propTypes = {
   ownerUsername: PropTypes.string.isRequired,
   enableTweet: PropTypes.bool.isRequired,
 }
+
+export default BodyContainer
+
--- src/components/MainPanel.js
+++ src/components/MainPanel.js
@@ -1,63 +1,25 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import TweetList from './TweetList'
 import NewTweet from './NewTweet'
-import config from '../config'
 
-class MainPanel extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: 'Nattanicha',
-      username: 'nattyait',
-      tweets: [],
-    }
-    this.addToTweetList = this.addToTweetList.bind(this)
-  }
-  componentDidMount() {
-    const uri = `http://${config.api.host}:${config.api.port}/api/tweets`
-    const filter = `{"where":{"username":"${this.state.username}"}}`
-
-    fetch(`${uri}?filter=${filter}`, {
-      mode: 'cors',
-    })
-      .then(response => response.json())
-      .then((tweets) => {
-        this.setState({
-          tweets, // tweets: tweets
-        })
-      })
-  }
-  addToTweetList(tweet) {
-    const tweetWithId = tweet
-    tweetWithId.id = this.state.tweets.length
-
-    this.setState({
-      tweets: [
-        ...this.state.tweets,
-        tweetWithId,
-      ],
-    })
-  }
-
-  render() {
-    const { name, username, tweets } = this.state
-
-    return (
-      <div className="main-panel">
-        { this.props.enableTweet
-        ? <NewTweet
-          name={name}
-          username={username}
-          addToTweetList={this.addToTweetList}
-        />
-        : null }
-        <TweetList tweets={tweets} />
-      </div>
-    )
-  }
-}
+const MainPanel = ({ name, username, tweets, enableTweet, addToTweetList }) => (
+  <div className="main-panel">
+    { enableTweet
+    ? <NewTweet
+      name={name}
+      username={username}
+      addToTweetList={addToTweetList}
+    />
+    : null }
+    <TweetList tweets={tweets} />
+  </div>
+)
 
 MainPanel.propTypes = {
   enableTweet: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  tweets: PropTypes.arrayOf(PropTypes.object).isRequired,
+  addToTweetList: PropTypes.func.isRequired,
 }
 export default MainPanel
